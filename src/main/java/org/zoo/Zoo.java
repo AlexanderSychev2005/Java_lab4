c07559856077df676f9d154c85c2a8e46faaf30d
Alex <alexandersychev2005@gmail.com>
2025-10-22 09:34:42 +0300

    Updated comments

--- src/main/java/org/zoo/Zoo.java
+++ src/main/java/org/zoo/Zoo.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Zoo {
-    public List<Cage<? extends Animal>> cages = new ArrayList<>();
+    public List<Cage<? extends Animal>> cages = new ArrayList<>(); // Generic list of cages for any type of Animal
 
     public void addNewCage(Cage<? extends Animal> cage) {
         cages.add(cage);
